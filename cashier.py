9d70605e655fd00bdec4c98a0fbfd7cb5c3426af
lbetanc21 <lbetanc2@charlotte.edu>
2026-06-10 14:50:34 -0400

    Update cashier.py

--- cashier.py
+++ cashier.py
@@ -1,1 +1,29 @@
+class Cashier:
+    def __init__(self):
+        pass
 
+    def process_coins(self):
+        """Returns the total calculated from coins inserted."""
+        print("Please insert coins.")
+        large_dollars = int(input("How many large dollars?: "))
+        half_dollars = int(input("How many half dollars?: "))
+        quarters = int(input("How many quarters?: "))
+        nickels = int(input("How many nickels?: "))
+
+        total = 0
+        total += large_dollars * 1.00
+        total += half_dollars * 0.50
+        total += quarters * 0.25
+        total += nickels * 0.05
+
+        return total
+
+    def transaction_result(self, coins, cost):
+        """Return True when payment is accepted, or False if money is insufficient."""
+        if coins >= cost:
+            change = round(coins - cost, 2)
+            print(f"Here is ${change} in change.")
+            return True
+        else:
+            print("Sorry that's not enough money. Money refunded.")
+            return False
